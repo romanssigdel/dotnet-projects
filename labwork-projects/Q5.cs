5b4be64b9b638057619330b4f485d0d0fbd575fc
Romans Sigdel <romanssigdel8@gmail.com>
2024-03-23 19:36:57 +0545

    add inheritance

---
+++ labwork-projects/Q5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labwork_projects
+{
+    //Multi level iheritace and multiple inheritance
+    public class Animal
+    {
+        public string name = "Animal";
+        public void eats()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+    public class Dog:Animal {
+    public void barks()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+    public class BabyDog:Dog
+    {
+        public void crys()
+        {
+            Console.WriteLine("Crying....");
+        }
+    }
+    public class Cat:Animal
+    {
+        public void meows()
+        {
+            Console.WriteLine("Meowing...");
+        }
+    }
+    public class BabyCat: Cat
+    {
+        public void rollsUp()
+        {
+            Console.WriteLine("Rolling...");
+        }
+    }
+}
